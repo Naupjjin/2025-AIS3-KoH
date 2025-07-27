2f549039c9b16a6e1fbc83ad51035e630dc012f9
ItisCaleb <40360126+ItisCaleb@users.noreply.github.com>
2025-07-27 19:20:05 +0800

    Add web api

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/simulate")
+def simulate():
+    return ""
+
+@app.route("/")
+def root():
+    return "hello"
+
+if __name__ == "__main__":
+    app.run()
