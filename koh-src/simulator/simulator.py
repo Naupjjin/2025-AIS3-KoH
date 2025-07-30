d485da1482ce15d00b2f12d9f9da6b2e8648d1fb
naup96321 <naup96721@gmail.com>
2025-07-30 08:38:06 +0800

    treashure ecc

--- koh-src/simulator/simulator.py
+++ koh-src/simulator/simulator.py
@@ -36,7 +36,59 @@ class Chest:
         pass
     def point_addition_chal(self):
         self.score = 60
-        pass
+
+        # y^2 = x^3 + a*x + b (mod p)
+        p = 9739
+        a, b = 3, 7
+
+        def inv_mod(x, m):
+            return pow(x, -1, m)
+
+        def is_on_curve(x, y):
+            return (y * y - (x * x * x + a * x + b)) % p == 0
+
+        def point_add(P, Q):
+            x1, y1 = P
+            x2, y2 = Q
+
+            if x1 == x2 and (y1 + y2) % p == 0:
+                return None  
+
+            if P != Q:
+                lam = ((y2 - y1) * inv_mod((x2 - x1) % p, p)) % p
+            else:
+                if y1 == 0:
+                    return None 
+                lam = ((3 * x1 * x1 + a) * inv_mod((2 * y1) % p, p)) % p
+
+            x3 = (lam * lam - x1 - x2) % p
+            y3 = (lam * (x1 - x3) - y1) % p
+            return (x3, y3)
+
+        def random_point():
+            while True:
+                x = random.randint(0, p - 1)
+                rhs = (x**3 + a*x + b) % p
+            
+                if pow(rhs, (p-1)//2, p) != 1:
+                    continue  
+                for y in range(p):
+                    if (y*y) % p == rhs:
+                        return (x, y)
+
+        while True:
+            P = random_point()
+            Q = random_point()
+            R = point_add(P, Q)
+            if R is not None:
+                break
+
+        # print(f"P = {P}")
+        # print(f"Q = {Q}")
+        # print(f"P + Q = {R}")
+        # a, b, p, px, py, qx, qy
+        self.param = [a, b, p, P[0], P[1], Q[0], Q[1]]
+        self.result = [R[0], R[1]]
     
     CHALS = [reverse_chal, sort_chal, rsa_chal, point_addition_chal]
 
--- task_example/ecc.py
+++ task_example/ecc.py
@@ -1,44 +1,51 @@
-class Point:
-    def __init__(self, x, y, p):
-        self.x = x % p
-        self.y = y % p
-        self.modulus = p
+import random
 
-def inv_mod(x, p):
-    return pow(x, -1, p)
-
-def addition(p1: Point, p2: Point, a, b):
-    p = p1.modulus
-    x1, y1 = p1.x, p1.y
-    x2, y2 = p2.x, p2.y
-
-    if x1 == x2 and y1 == y2:
-
-        numerator = (3 * x1 * x1 + a) % p
-        denominator = (2 * y1) % p
-        d = numerator * inv_mod(denominator, p) % p
-    else:
- 
-        numerator = (y2 - y1) % p
-        denominator = (x2 - x1) % p
-        d = numerator * inv_mod(denominator, p) % p
-
-    x3 = (d*d - x1 - x2) % p
-    y3 = (d * (x1 - x3) - y1) % p
-    return Point(x3, y3, p)
-
-def elliptic_addition(a, b, p, P_x, P_y, Q_x, Q_y):
-    P = Point(P_x, P_y, p)
-    Q = Point(Q_x, Q_y, p)
-    R = addition(P, Q, a, b)
-    return R.x, R.y
-
-
-a = 497
-b = 1768
+# y^2 = x^3 + a*x + b (mod p)
 p = 9739
-P_x, P_y = 493, 5564
-Q_x, Q_y = 1539, 4742
+a, b = 3, 7
 
-x3, y3 = elliptic_addition(a, b, p, P_x, P_y, Q_x, Q_y)
-print(x3, y3)
+def inv_mod(x, m):
+    return pow(x, -1, m)
+
+def is_on_curve(x, y):
+    return (y * y - (x * x * x + a * x + b)) % p == 0
+
+def point_add(P, Q):
+    x1, y1 = P
+    x2, y2 = Q
+
+    if x1 == x2 and (y1 + y2) % p == 0:
+        return None  
+
+    if P != Q:
+        lam = ((y2 - y1) * inv_mod((x2 - x1) % p, p)) % p
+    else:
+        if y1 == 0:
+            return None 
+        lam = ((3 * x1 * x1 + a) * inv_mod((2 * y1) % p, p)) % p
+
+    x3 = (lam * lam - x1 - x2) % p
+    y3 = (lam * (x1 - x3) - y1) % p
+    return (x3, y3)
+
+def random_point():
+    while True:
+        x = random.randint(0, p - 1)
+        rhs = (x**3 + a*x + b) % p
+    
+        if pow(rhs, (p-1)//2, p) != 1:
+            continue  
+        for y in range(p):
+            if (y*y) % p == rhs:
+                return (x, y)
+
+while True:
+    P = random_point()
+    Q = random_point()
+    R = point_add(P, Q)
+    if R is not None:
+        break
+
+print(f"P = {P}")
+print(f"Q = {Q}")
+print(f"P + Q = {R}")
